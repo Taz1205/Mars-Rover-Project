146964d432472a104cbbb54f86e209743fa8022e
Fatima Zahra <fathima_z@yahoo.com>
2023-06-10 16:35:46 +0100

    More test passed to check square or rectangle grid

--- src/mars_rover.test.ts
+++ src/mars_rover.test.ts
@@ -1,7 +1,20 @@
-import {initialPosition } from "./mars_rover";
+import {
+  executeInstructions, initialPosition
+  
+} from "./mars_rover";
 
 describe("Mars Rover Functionality", () => {
+  describe("Rover Initial Position", () => {
   it("should return the rover's initial position at 0 0 N", () => {
-    expect(initialPosition()).toBe("0 0 N");
+    expect(initialPosition()).toBe('0 0 N');
   });
+});
+describe("Grid Size or Plateau Size", () => {
+  it("should return the plateau gridsize for a square", () => {
+    expect(executeInstructions(['20 20'])).toBe('20 20');
+  });
+  it("should return the plateau gridsize for a rectangle", () => {
+    expect(executeInstructions(['8 6'])).toBe('8 6');
+  });
+});
 });
