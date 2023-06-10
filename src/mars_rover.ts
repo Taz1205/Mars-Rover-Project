648bc29ecee52e770ea6861549e5e57ce3f9e2c3
Fatima Zahra <fathima_z@yahoo.com>
2023-06-10 16:32:13 +0100

    Second and third test passed for initial grid size

--- src/mars_rover.ts
+++ src/mars_rover.ts
@@ -1,3 +1,11 @@
 export function initialPosition() {
   return '0 0 N';
 }
+
+export function executeInstructions(input: string[]){
+  const plateauSize = input[0].split(" ").map(Number);
+  const plateauWidth = plateauSize[0];
+  const plateauHeight = plateauSize[1]; 
+  
+  return plateauWidth + " " + plateauHeight;
+}
